bb388bf91d16aa66891f72f95ce2d3fa9c16daeb
mbashia <mbashiavictor@gmail.com>
2024-09-11 04:41:12 +0300

    Live view intergration test

--- test/live/weather_live_test.exs
+++ test/live/weather_live_test.exs
@@ -5,17 +5,15 @@ defmodule WeatherAppWeb.WeatherLiveTest do
 
   describe "search form" do
     test "displays the search form", %{conn: conn} do
-      {:ok, view, html} = live(conn, ~p"/weather" )
+      {:ok, view, html} = live(conn, ~p"/weather")
 
-       
       assert html =~ "Weather App"
 
+      assert view
+             |> element("#search-button", "Get Weather")
+             |> has_element?()
+    end
 
    
-      assert view
-      |> element("#search-button", "Get Weather")
-      |> has_element?()
-
-    end
   end
 end
